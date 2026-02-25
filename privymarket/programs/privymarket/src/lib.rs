650904016be13bce8489e95c9b4310c4a76baecd
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2026-02-25 23:37:59 +0530

    chore : iniitialised anchor project

---
+++ privymarket/programs/privymarket/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5UAhemfaML4RSGQ6GvDYLBHBC4JdAAQ2qZWdQi7LXgdr");
+
+#[program]
+pub mod privymarket {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
